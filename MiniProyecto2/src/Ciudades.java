524f9ba9f2a034638ab42f0d424b0f508e071c4b
juangabrielparedes <juan.gabriel.paredes@correounivalle.edu.co>
2023-11-16 16:20:19 -0500

    insertar clases y enums

---
+++ MiniProyecto2/src/Ciudades.java
@@ -0,0 +1,22 @@
+public enum Ciudades {
+    Cali, 
+    Buenaventura, 
+    Palmira,
+    Tuluá,
+    Buga,
+    Cartago,
+    Yumbo,
+    Jamundí,
+    Dagua,
+    LaCumbre,
+    Zarzal,
+    ElCerrito,
+    Ginebra,
+    Sevilla,
+    Caicedonia,
+    Roldanillo,
+    Trujillo,
+    Andalucía,
+    Versalles,
+}
+
